37724fd1446b1c711b7475109ef320fb1495594d
micouy <szpontaniczny@gmail.com>
2019-07-27 18:58:35 +0200

    Add /startsearch command

--- src/com/mikolaj/SearchForMonumentPlugin.java
+++ src/com/mikolaj/SearchForMonumentPlugin.java
@@ -1,11 +1,21 @@
 package com.mikolaj;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SearchForMonumentPlugin extends JavaPlugin {
+    private CalcDistanceTask task;
+
 	@Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new JoinListener(this), this);
-        getServer().getPluginManager().registerEvents(new MoveListener(), this);
+		this.getCommand("startsearch").setExecutor(new StartSearchCommand(this));
     }
+	  
+    public void startTrackingMovement(Player player) {
+		this.task = new CalcDistanceTask(player);
+        this.task.runTaskTimer(this, 0L, 60L);
+
+		getLogger().info("Started tracking movement");
+        getServer().getPluginManager().registerEvents(new MoveListener(), this);
+	}
 }
---
+++ src/com/mikolaj/StartSearchCommand.java
@@ -0,0 +1,34 @@
+package com.mikolaj;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class StartSearchCommand implements CommandExecutor {
+	private final SearchForMonumentPlugin plugin;
+	
+	public StartSearchCommand(SearchForMonumentPlugin plugin) {
+		this.plugin = plugin;
+	}
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    	if (args.length != 1) {
+    		return false;
+    	}
+    	
+    	Player player = Bukkit.getPlayer(args[0]);
+    	
+    	if (player == null) {
+    		return false;
+    	}
+
+    	this.plugin.startTrackingMovement(player);
+    	new InventoryHandler().equipPlayer(player);
+    	
+        return true;
+    }
+  
+}
